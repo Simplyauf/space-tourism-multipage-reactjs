0d09f8e757ab5b6d7f5f7fd49f1788b7e44d5763
unknown <azeezumarfaruk@gmail.com>
2022-09-03 21:05:07 +0100

    earlier branch

--- src/components/destinationpage/OverallDestinationPage.js
+++ src/components/destinationpage/OverallDestinationPage.js
@@ -3,66 +3,91 @@ import "../../index.css";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { DestinationPageTabs } from "./destinationPageTabs";
-
 import { data } from "../data";
+import { motion } from "framer-motion";
+import { useAnimateElemInView } from "../../utils/useAnimateElemInView";
 
 const DestinationPage = ({ entireSectionParentElement }) => {
-	const [destinationPageDatas, setDestinationPageDatas] = useState({ name: "", images: "", description: "", distance: "", travel: "" });
-	const { name, images, description, distance, travel } = destinationPageDatas;
+  const [destinationPageDatas, setDestinationPageDatas] = useState({
+    name: "",
+    images: "",
+    description: "",
+    distance: "",
+    travel: "",
+  });
+  const { name, images, description, distance, travel } = destinationPageDatas;
 
-	if (entireSectionParentElement) {
-		entireSectionParentElement.id = "destinationsPage";
-	}
+  if (entireSectionParentElement) {
+    entireSectionParentElement.id = "destinationsPage";
+  }
 
-	useEffect(() => {
-		setDestinationPageDatas(data.destinations[0]);
-		console.log(data.destinations[0]);
-	}, []);
+  useEffect(() => {
+    setDestinationPageDatas(data.destinations[0]);
+    console.log(data.destinations[0]);
+  }, []);
 
-	const handleTabsClick = (e) => {
-		e.currentTarget.parentElement.childNodes.forEach((destinationTabsElemContainer) => {
-			destinationTabsElemContainer.classList.remove("active");
-		});
+  //use animate hooks
+  const [ref, control, visibleVariant] = useAnimateElemInView();
 
-		e.currentTarget.classList.add("active");
+  const handleTabsClick = (e) => {
+    e.currentTarget.parentElement.childNodes.forEach((destinationTabsElemContainer) => {
+      destinationTabsElemContainer.classList.remove("active");
+    });
 
-		const ObjOfTheTabClicked = data.destinations.find((destinationData, index) => {
-			return e.currentTarget.dataset.id === destinationData.name;
-		});
-		console.log(ObjOfTheTabClicked);
-		setDestinationPageDatas(ObjOfTheTabClicked);
-	};
+    e.currentTarget.classList.add("active");
 
-	return (
-		<main class="lg:flex lg:w-[90%] lg:justify-between lg:mx-auto lg:gap-[5%]">
-			<article class="lg:w-[50%]">
-				<li class="mt-6 py-1 text-center tablet:text-left tablet:ml-[10%] md:ml-[10%] md:text-left lg:py-4 text-[28px] font-Barlow">
-					<span class="opacity-50 text-secondaryColor">01</span>
-					<span class="ml-2 text-secondaryColor tracking-[2.75px]">PICK YOUR DESTINATION</span>
-				</li>
-				<div class="w-[60%] h-[220px] tablet:h-[250px] mdHeight:h-[280px] lgHeight:h-[310px] mt-4 lg:mt-16 mx-auto">
-					<img class="max-w-[100%] max-h-[100%] mx-auto current-tab-img" src={images.png} alt="destination-img" />
-				</div>
-			</article>
-			<article class="lg:w-[50%] lg:flex lg:flex-col lg:items-start lg:mt-28">
-				<DestinationPageTabs handleTabsClick={handleTabsClick} />
+    const ObjOfTheTabClicked = data.destinations.find((destinationData, index) => {
+      return e.currentTarget.dataset.id === destinationData.name;
+    });
+    console.log(ObjOfTheTabClicked);
+    setDestinationPageDatas(ObjOfTheTabClicked);
+  };
 
-				<h1 class="text-center text-[80px] font-Bellefair mt-4 current-tab-title uppercase lg:text-[100px] lg:mt-0">{name}</h1>
+  return (
+    <main class="lg:flex lg:w-[90%] lg:justify-between lg:mx-auto lg:gap-[5%]">
+      <article class="lg:w-[50%]">
+        <li class="mt-6 py-1 text-center tablet:text-left tablet:ml-[10%] md:ml-[10%] md:text-left lg:py-4 text-[28px] font-Barlow">
+          <span class="opacity-50 text-secondaryColor">01</span>
+          <span class="ml-2 text-secondaryColor tracking-[2.75px]">PICK YOUR DESTINATION</span>
+        </li>
+        <div class="w-[57%] h-[227px] tablet:h-[250px] mdHeight:h-[280px] lgHeight:h-[310px] mt-4 lg:mt-16 mx-auto">
+          <motion.img
+            ref={ref}
+            initial="hidden"
+            animate={control}
+            transition={{ type: "spring", stiffness: 100 }}
+            variants={visibleVariant}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            class="w-[100%] h-[100%] mx-auto current-tab-img"
+            src={images.png}
+            alt="destination-img"
+          />
+        </div>
+      </article>
+      <article class="lg:w-[50%] lg:flex lg:flex-col lg:items-start lg:mt-28">
+        <DestinationPageTabs handleTabsClick={handleTabsClick} />
 
-				<p class="w-[80%] mx-auto pb-6 border-opacity-50 border-b-2 border-white mb-6 mt-4 text-center text-[16px] leading-[30px] font-Barlow text-primaryColor current-tab-description lg:mt-0 lg:text-left lg:mx-0 lg:text-[18px] tracking-[1px]">{description}</p>
-				<div class="mt-4 tablet:flex tablet:flex-row tablet:justify-center tablet:gap-[5%] lg:flex-row lg:flex laptop:gap-[15%] lg:gap-[10%] lg:w-[100%]">
-					<div class="mb-8 text-center">
-						<h2 class="text-[14px] font-Barlow tracking-[2.35px] text-primaryColor">AVG. DISTANCE</h2>
-						<span class="text-[28px] font-Bellefair current-tab-distance uppercase"> {distance}</span>
-					</div>
-					<div class="text-center">
-						<h2 class="text-[14px] font-Barlow tracking-[2.35px] text-primaryColor">EST. TRAVEL TIME</h2>
-						<span class="text-[28px] font-Bellefair current-tab-travelTime uppercase">{travel}</span>
-					</div>
-				</div>
-			</article>
-		</main>
-	);
+        <h1 class="text-center text-[80px] font-Bellefair mt-4 current-tab-title uppercase lg:text-[100px] lg:mt-0">
+          {name}
+        </h1>
+
+        <p class="w-[80%] mx-auto pb-6 border-opacity-50 border-b-2 border-white mb-6 mt-4 text-center text-[16px] leading-[30px] font-Barlow text-primaryColor current-tab-description lg:mt-0 lg:text-left lg:mx-0 lg:text-[18px] tracking-[1px]">
+          {description}
+        </p>
+        <div class="mt-4 tablet:flex tablet:flex-row tablet:justify-center tablet:gap-[5%] lg:flex-row lg:flex laptop:gap-[15%] lg:gap-[10%] lg:w-[100%]">
+          <div class="mb-8 text-center">
+            <h2 class="text-[14px] font-Barlow tracking-[2.35px] text-primaryColor">AVG. DISTANCE</h2>
+            <span class="text-[28px] font-Bellefair current-tab-distance uppercase"> {distance}</span>
+          </div>
+          <div class="text-center">
+            <h2 class="text-[14px] font-Barlow tracking-[2.35px] text-primaryColor">EST. TRAVEL TIME</h2>
+            <span class="text-[28px] font-Bellefair current-tab-travelTime uppercase">{travel}</span>
+          </div>
+        </div>
+      </article>
+    </main>
+  );
 };
 
 export default DestinationPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-	theme: {
-		extend: {
-			colors: {
-				primaryColor: "hsl(231, 77%, 90%)",
-				secondaryColor: "hsl(0, 0%, 100%)",
-				neutralColor: "hsl(230, 35%, 7%)",
-			},
-			fontFamily: {
-				Bellefair: ["Bellefair", "serif"],
-				Barlow: ["Barlow Condensed", "sans-serif"],
-			},
-			screens: {
-				laptop: "1300px",
-				tablet: { min: "520px", max: "767px" },
-				mdHeight: { raw: "(min-height:700px)" },
-				lgHeight: { raw: "(min-height:850px)" },
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "hsl(231, 77%, 90%)",
+        secondaryColor: "hsl(0, 0%, 100%)",
+        neutralColor: "hsl(230, 35%, 7%)",
+      },
+      fontFamily: {
+        Bellefair: ["Bellefair", "serif"],
+        Barlow: ["Barlow Condensed", "sans-serif"],
+      },
+      screens: {
+        laptop: "1300px",
+        tablet: { min: "480px", max: "767px" },
+        mdHeight: { raw: "(min-height:700px)" },
+        lgHeight: { raw: "(min-height:850px)" },
+      },
+    },
+  },
+  plugins: [],
 };
