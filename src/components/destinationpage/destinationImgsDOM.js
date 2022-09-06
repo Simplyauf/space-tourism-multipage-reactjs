82847d83b1ce4bcf0281ab599e2a5fc229634222
unknown <azeezumarfaruk@gmail.com>
2022-09-06 16:18:05 +0100

    style:Moved styles of IMG section of pages and also added animation style to IMG-section of some pages

---
+++ src/components/destinationpage/destinationImgsDOM.js
@@ -0,0 +1,43 @@
+import { data } from "../data";
+import "../../index.css";
+import { motion } from "framer-motion";
+
+export const DestinationImg = ({ currentImgUrl }) => {
+  console.log(currentImgUrl);
+
+  return (
+    <div class="w-[53%] tablet:w-[41%] md:w-[37%] lg:w-[60%] min-h-[227px] tablet:min-h-[235px]  mdHeight:h-[280px] lgHeight:h-[310px] mt-4 lg:mt-16 mx-auto relative">
+      {data.destinations.map((destinationData) => {
+        const { images, name } = destinationData;
+        return (
+          <motion.img
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            className={`${
+              images.png === currentImgUrl
+                ? "current-destinationPage-img-tab destinationPage-img-tab"
+                : "destinationPage-img-tab"
+            }`}
+            src={images.png}
+            alt={name}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+// {images.png && (
+//     <motion.img
+//       ref={ref}
+//       initial="hidden"
+//       animate={control}
+//       variants={visibleVariant}
+//       transition={{ type: "spring", stiffness: 100 }}
+//       whileHover={{ scale: 1.05 }}
+//       whileTap={{ scale: 0.9 }}
+//       class="w-[100%] h-[100%] mx-auto current-tab -img"
+//       src={images.png}
+//       alt="destination-img"
+//     />
+//   )}
